aa33c927bad728f8c785ec8fd50ab0231991eda0
Tom Aalbers <dtaalbers@gmail.com>
2016-04-24 15:00:15 +0200

    Initital file transfer example commit

---
+++ source/app/pages/uploading/uploading.ts
@@ -0,0 +1,90 @@
+import {Page, NavController, NavParams, Alert} from 'ionic-angular';
+import {Plugins} from '../../services/plugins.service';
+import {Home} from '../home/home';
+import * as $ from 'jquery';
+
+@Page({
+  templateUrl: 'build/pages/uploading/uploading.html',
+})
+export class UploadingPage {
+    
+    images: Array<string>
+    
+    finished: boolean = false;
+    current: number = 1;
+    total: number;
+    
+    constructor(private nav: NavController, 
+                private navParams: NavParams,
+                private plugins: Plugins) {  
+                    
+        this.images = this.navParams.get("images");        
+        if(!this.images || this.images.length == 0) {
+            let alert = Alert.create({
+                title: "Error",
+                subTitle: "No images found to upload",
+                buttons: ['Ok']
+            });
+            nav.present(alert);
+            return;
+        }      
+        
+        this.total = this.images.length;   
+        this.upload(this.images[0]);       
+    }
+    
+    done = () : void => {
+        this.nav.setRoot(Home);    
+    }
+    
+    success = (result: any) : void => {            
+        this.current++;         
+        if(this.current <= this.total) {              
+            this.setCurrent(this.current);   
+            this.setProgress(0);            
+            setTimeout(() : void => {     
+                // give the animation time to reset           
+                this.upload(this.images[this.current - 1]);
+            },1000);
+        } else {   
+            this.finished = true;
+            $("#done").show();
+        }
+    }
+            
+    failed = (err: any) : void => {
+        var code = err.code;
+        console.log(err);
+        alert("Failed to upload image. Code: " + code);
+    }
+    
+    onProgress =  (progressEvent: ProgressEvent) : void => {
+        if (progressEvent.lengthComputable) {
+            var progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+            this.setProgress(progress);       
+        }
+    }
+    
+    setProgress = (progress: number) : void => {  
+        var $circle = $('#svg #bar');
+        if(progress < 0 ){
+            progress = 0;
+        }
+        if(progress > 100) {
+            progress = 100;
+        }         
+        let r = parseInt($circle.attr('r'));
+        var c = Math.PI*(r*2);
+        var pct = ((100-progress)/100)*c;            
+        $circle.css({ strokeDashoffset: pct});            
+        $('#cont').attr('data-pct',progress);              
+    }
+    
+    setCurrent = (current: number) : void => {
+        $("#current").html(current.toString());
+    }
+    
+    upload = (image: string) : void => { 
+        this.plugins.file.upload("http://services.dtaalbers.com/staging/pictures", image, this.success, this.failed, this.onProgress);
+    }
+}
---
+++ source/app/services/plugins.service.ts
@@ -0,0 +1,73 @@
+import {Injectable} from "angular2/core";
+import {Camera, ImagePicker} from 'ionic-native';
+import * as _ from 'underscore';
+
+@Injectable()
+export class Plugins {
+    
+    constructor() { }     
+    
+    albums = {            
+        open () : Promise<any>  { 
+            return ImagePicker.getPictures({
+                    quality: 100,                        
+                    maximumImagesCount: 15,
+            }).then((imgUrls) => {
+                console.log(imgUrls);
+                return imgUrls;
+            }, (err) => {                                   
+                if(err.error == "cordova_not_available") {               
+                    alert("Cordova is not available, please make sure you have your app deployed on a simulator or device");                                   
+                } else {                
+                    alert("Failed to open albums: " + err.error);
+                }
+            });
+        },         
+    }
+    
+    camera = {       
+        open () : Promise<any>  {
+            var options = {
+                destinationType: 1,
+                sourceType: 1,
+                encodingType: 0,
+                quality:100,
+                allowEdit: false,
+                saveToPhotoAlbum: true,            
+                correctOrientation: true,
+            };        
+            return Camera.getPicture(options).then((imgUrl) => {
+                console.log(imgUrl);
+                return imgUrl;
+            }, (err) => {                
+                if(err.error == "cordova_not_available") {
+                    alert("Cordova is not available, please make sure you have your app deployed on a simulator or device");            
+                } else {
+                    alert("Failed to open camera: " + err.error);                
+                }    
+            });
+        } 
+    }  
+    
+    file = {
+        upload (url: string, image: string, onSuccess: any, onFailed: any, onProgress: any) : void {
+            var ft = new FileTransfer();                       
+            var options = new FileUploadOptions();
+            var filename = _.uniqueId();
+            
+            options.fileKey = "file";
+            options.fileName = filename;
+            options.mimeType = "image/jpeg"
+            options.chunkedMode = false;
+            options.headers = { 
+                'Content-Type' : undefined
+            }                 
+             options.params = {
+                fileName: filename
+            };  
+                   
+            ft.onprogress =  (e: ProgressEvent) => onProgress(e);  
+            ft.upload(image, url, onSuccess, onFailed, options);            
+        }
+    }  
+}
