23ea173c37e9d84773982d7b797ebec9b5dfebb2
Tom Aalbers <dtaalbers@gmail.com>
2016-06-01 22:05:55 +0200

    Forgot to commit some small fixed for the update to beta 7

--- source/typings/main.d.ts
+++ source/typings/main.d.ts
@@ -1,6 +1,4 @@
 /// <reference path="main/ambient/cordova/index.d.ts" />
-/// <reference path="main/ambient/cordova/plugins/filesystem/index.d.ts" />
-/// <reference path="main/ambient/cordova/plugins/filetransfer/index.d.ts" />
 /// <reference path="main/ambient/es6-shim/index.d.ts" />
 /// <reference path="main/ambient/jquery/index.d.ts" />
 /// <reference path="main/ambient/underscore/index.d.ts" />
