331617b7001d21e8127cd7f40033cfa12e6d16d1
Tom Aalbers <dtaalbers@gmail.com>
2017-06-03 16:42:57 +0200

    [cleanup] Removed old ionic 2 example projects

--- file-transfer-download/typings/main.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="main/ambient/es6-shim/index.d.ts" />
-/// <reference path="globals/cordova/index.d.ts" />
-/// <reference path="globals/cordova/plugins/filesystem/index.d.ts" />
--- file-transfer-upload/typings/browser.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="browser/ambient/cordova/index.d.ts" />
-/// <reference path="browser/ambient/cordova/plugins/filesystem/index.d.ts" />
-/// <reference path="browser/ambient/cordova/plugins/filetransfer/index.d.ts" />
-/// <reference path="browser/ambient/es6-shim/index.d.ts" />
-/// <reference path="browser/ambient/jquery/index.d.ts" />
-/// <reference path="browser/ambient/underscore/index.d.ts" />
