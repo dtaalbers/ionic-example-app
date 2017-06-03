2c5fcaa7d3b3c58c67cdeb63703a194bc1843a69
Tom Aalbers <dtaalbers@gmail.com>
2017-06-03 16:57:58 +0200

    [cleanup] Nicely formatted the default pages

--- src/src/pages/home/home.ts
+++ src/src/pages/home/home.ts
@@ -2,13 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage { }
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
-}
--- src/src/pages/tabs/tabs.ts
+++ src/src/pages/tabs/tabs.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
-
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 
 @Component({
-  templateUrl: 'tabs.html'
+    templateUrl: 'tabs.html'
 })
 export class TabsPage {
-
-  tab1Root = HomePage;
-
-  constructor() {
-
-  }
+    /**
+     * The first tab will be the homepage
+     */
+    tab1Root = HomePage;
 }
